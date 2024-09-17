1081dfe785a3b9d4bde16abedcebbe84750c0599
Kevin Varela <kevinvarelatalamantes@gmail.com>
2024-09-16 22:02:31 -0600

    Update Main.java
    Added comments, changed output display

--- challenge/src/Main.java
+++ challenge/src/Main.java
@@ -2,15 +2,20 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        // Array
         int[] array = {5, 3, 8, 7, 9};
+        // Sort the array in ascending order
         Arrays.sort(array);
-
+        // Get the array lenght
         int length = array.length;
 
-        int elementOne = array[length - 2];
-        int elementSecond = array[length - 1];
-
+        int elementOne = array[length - 1]; // Largest number
+        int elementSecond = array[length - 2]; // Second largest number
         int biggest = elementOne * elementSecond;
+
+        //Two numbers multiplied
+        System.out.println("Numbers multiplied: " + elementOne + " and " + elementSecond);
+        // Biggest product posible
         System.out.println("Biggest product = " + biggest);
     }
 }
