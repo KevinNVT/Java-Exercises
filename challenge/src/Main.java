97287f35354795cb4e47087c8ccf6ad80eafee96
Kevin Varela <kevinvarelatalamantes@gmail.com>
2024-09-16 22:05:13 -0600

    Update Main.java
    Added challenge description.

--- challenge/src/Main.java
+++ challenge/src/Main.java
@@ -1,3 +1,7 @@
+/* For a given input array of numbers, find the two that result in the largest product. 
+The ouput should include the two numbers in the array along with their product.
+As an extra challenge, use an input of two arrays of numbers and find two numbers - one from each input array - that results in the largest product.
+*/
 import java.util.Arrays;
 
 public class Main {
