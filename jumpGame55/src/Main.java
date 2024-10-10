6a98756d5f2aaedff3e46fd43288fefde249c6d4
Kevin Varela <kevinvarelatalamantes@gmail.com>
2024-10-10 16:13:49 -0600

    Update Main.java

--- jumpGame55/src/Main.java
+++ jumpGame55/src/Main.java
@@ -3,7 +3,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int n = nums.length;
         int goal = n - 1;
-
+        
         for (int i = n - 2; i >= 0; i--) {
             if (i + nums[i] >= goal) {
                 goal = i;
